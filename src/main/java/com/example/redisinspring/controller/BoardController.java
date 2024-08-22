3995f66a325eb0cbd14b9f84bf539d05e02680b7
jungwon <mac2001035@gmail.com>
2024-08-23 01:11:11 +0900

    FIX: 오류 수정.

--- src/main/java/com/example/redisinspring/controller/BoardController.java
+++ src/main/java/com/example/redisinspring/controller/BoardController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("boards")
+@RequestMapping("/boards")
 public class BoardController {
     private BoardService boardService;
 
--- src/main/java/com/example/redisinspring/entity/Board.java
+++ src/main/java/com/example/redisinspring/entity/Board.java
@@ -22,7 +22,7 @@ public class Board {
     private String content;
 
     @CreatedDate
-    @JsonFormat(pattern = "yyyy-MM-dd'T HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @JsonSerialize(using= LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
