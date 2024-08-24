d3b09edcbcead14af6de9881cb2f1394453ca69e
jungwon <mac2001035@gmail.com>
2024-08-24 17:00:04 +0900

    FEAT: Redis를 적용한 Cache 기법.

--- src/main/java/com/example/redisinspring/config/RedisConfig.java
+++ src/main/java/com/example/redisinspring/config/RedisConfig.java
@@ -1,6 +1,6 @@
 package com.example.redisinspring.config;
 
-import io.lettuce.core.dynamic.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
